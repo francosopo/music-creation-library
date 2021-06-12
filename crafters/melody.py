1dc5e051df8ea2f77b3174ef1e209e5ef625ce8a
Franco Seguel <francoseguellucero@gmail.com>
2021-06-12 16:40:04 -0400

    cargar melodia desde csv finalizado

--- crafters/melody.py
+++ crafters/melody.py
@@ -1,5 +1,4 @@
-from wave import open
-from struct import pack
+import csv, os, pathlib
 
 class Melody(object):
     
@@ -16,4 +15,25 @@ class Melody(object):
     
     def get_melody(self):
         return self.melody
+        
+
+class MelodyCSVLoader(Melody):
+
+    def __init__(self, scale, csv_dir):
+        super().__init__(scale)
+        self.csv_dir = csv_dir
+
+    def load_csv(self,name,column_names=False):
+        with open(os.path.join(self.csv_dir,f'{name}'), 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            if column_names:
+                line_count=0
+            else:
+                line_count = 1
+            
+            for row in csv_reader:
+                if line_count ==0:
+                    line_count += 1
+                else:
+                    self.use_note(int(row[0]), float(row[1]))
         
